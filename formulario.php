4415f90e5ad1a7dae607741f6db90c8bc35faa08
david <fremenida@gmail.com>
2020-07-10 16:32:57 +0200

    paso 1 creacion plugin

---
+++ formulario.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Plugin Name: Formulario Autoevaluacion
+Plugin URI: https://autoevaluacion.com/
+Description: Formulario Autoevaluacion
+Author: David
+Author URI: www.davidgilsoto.com
+License: GPLv2 or later
+Text Domain: formulario
+*/
+
+
+
